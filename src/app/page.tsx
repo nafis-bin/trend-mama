84830761c79ee11e29ee0724a6149e2a10851472
nafis bin <nafisbinnizam@gmail.com>
2025-09-21 04:24:28 +0600

    feat: cart section layout completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import ProductList from "@/components/ProductList"
 import Image from "next/image"
 
-const Homepage = () => {
+const Homepage = async ({ searchParams }
+  : {
+     searchParams: Promise<{ category: string }>
+  }) => {
+    
+  const category = (await searchParams).category
   return (
     <div className="">
       <div className="relative aspect-[3/1] mb-12">
@@ -11,7 +16,7 @@ const Homepage = () => {
           fill
          />
       </div>
-      <ProductList />
+      <ProductList category={category} params="homepage" />
     </div>
   )
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,6 +1,8 @@
 import { ProductsType } from "@/types"
 import Categories from "./Categories"
 import ProductCard from "./ProductCard"
+import Link from "next/link"
+import Filter from "./Filter"
 
 const products: ProductsType = [
   {
@@ -113,10 +115,11 @@ const products: ProductsType = [
   },
 ]
 
-const ProductList = () => {
+const ProductList = ({ category, params }: { category: string, params: "homepage" | "products" }) => {
     return (
         <div className="w-full">
             <Categories />
+            {params === "products" && <Filter />}
             <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
             gap-12">
                 {products.map((product, _) => (
@@ -126,6 +129,12 @@ const ProductList = () => {
                     />
                 ))}
             </div>
+            <Link 
+              href={category ? `/products/?category=${category}` : "/products"}
+              className="flex justify-end mt-4 underline text-sm text-gray-500"
+            >
+              View all products
+            </Link>
         </div>
     )
 }
